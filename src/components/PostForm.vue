ad824d94ea269bb370294808029f69b45fdd62c5
oskar shtefan <oskar.shtefan@gmail.com>
2022-06-05 00:16:12 +0600

    added new Component and modified

---
+++ src/components/PostForm.vue
@@ -0,0 +1,63 @@
+<template>
+    <form>
+        <input
+                v-model="post.title"
+                class="posts__inputAddPost"
+                type="text"
+                placeholder="Названте поста">
+        <input
+                v-model="post.description"
+                class="posts__inputAddPost"
+                type="text"
+                placeholder="Описание">
+        <input
+                v-bind:value="this.img"
+                @input="post.img = $event.target.value"
+                class="posts__inputAddPost"
+                type="text"
+                placeholder="Фото(ссылка)">
+        <button
+                @click="createPost(post)"
+                type="button"
+                class="posts__btnAddPost postItem__btn">Добавить
+        </button>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                post: {
+                    title: '',
+                    description: '',
+                    img: ''
+                }
+            }
+        },
+        props: {
+            posts: {
+                type: Array,
+                required: true,
+            }
+        },
+        methods: {
+            createPost(newPost) {
+                this.posts.push(newPost)
+                this.title = ''
+                this.description = ''
+                this.img = ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .posts__inputAddPost {
+        width: 60%;
+        padding: 5px;
+        margin-bottom: 15px;
+        border: 1px solid #3e8969;
+    }
+</style>
--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,14 +1,64 @@
 <template>
-
+    <div v-for="(post,i) in posts">
+        <li v-if="i < showCount" :key="post.id" class="content__postItem">
+            <div class="postItem__main-part">
+                <img class="postItem__img" :src="post.img" alt="post.title">
+                <div class="postItem__text">
+                    <h3>{{post.title}}</h3>
+                    <p>{{post.description}}</p>
+                    <a href="">подробнее</a>
+                </div>
+            </div>
+            <button class="postItem__btn">Нравиться</button>
+        </li>
+    </div>
+    <button v-if="showCount < posts.length" class="postItem__btn" @click="showMore">Смотреть больше..</button>
 </template>
-
 
 <script>
     export default {
-        name: "PostItem"
+        data() {
+            return {
+                showCount: 3,
+            }
+        },
+        props: {
+            posts: {
+                type: Array,
+                required: true,
+            }
+        },
+        methods: {
+            showMore() {
+                this.showCount += 3
+            }
+        }
     }
 </script>
 
 <style scoped>
+    .posts__posts-content {
+        width: 100%;
+        list-style: none;
+    }
 
+    .content__postItem {
+        padding: 15px;
+        margin-bottom: 20px;
+        display: flex;
+        justify-content: space-between;
+        border: 2px solid #3e8969;
+        align-items: center;
+    }
+
+    .postItem__img {
+        max-width: 150px;
+        margin-right: 15px;
+    }
+
+    .postItem__main-part {
+        max-width: 40%;
+        display: flex;
+        align-items: center;
+    }
 </style>
